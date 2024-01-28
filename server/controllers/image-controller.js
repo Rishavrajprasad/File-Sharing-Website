3e948ffdcc962092a1dd8ba1d7859b968693f51e
Rishavrajprasad <rishavrajprasad800@gmail.com>
2024-01-28 19:54:06 +0530

    error corrected

--- server/controllers/image-controller.js
+++ server/controllers/image-controller.js
@@ -9,7 +9,7 @@ export const uploadImage = async (request, response) => {
     
     try {
         const file = await File.create(fileObj);
-        response.status(200).json({ path: `https://fileflow-7qyv.onrender.com//file/${file._id}`});
+        response.status(200).json({ path: `https://fileflow-7qyv.onrender.com/file/${file._id}`});
     } catch (error) {
         console.error(error.message);
         response.status(500).json({ error: error.message });
